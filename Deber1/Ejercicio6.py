7eb20edf52a0b052009e4102b1d523acafbdfb43
Jostin <figueroajostin4@gmail.com>
2025-05-16 08:21:25 -0500

    actualizacion

--- Deber1/Ejercicio6.py
+++ Deber1/Ejercicio6.py
@@ -1,4 +1,7 @@
-##Escribir un programa que pida al usuario dos números enteros y muestre por pantalla la <n> entre <m> da un cociente <c> y un resto <r> donde <n> y <m> son los números introducidos por el usuario, y <c> y <r> son el cociente y el resto de la división entera respectivamente.
+##Escribir un programa que pida al usuario dos números
+# enteros y muestre por pantalla la <n> entre <m> da un cociente <c>
+# y un resto <r> donde <n> y <m> son los números introducidos por el usuario,
+# y <c> y <r> son el cociente y el resto de la división entera respectivamente.
 numero1 = int(input("Ingrese el primer valor: "))
 numero2 = int(input("Ingrese el segundo valor: "))
 cosciente = numero1/numero2
