ec0a56b4b730cc3ae6feff1c9ec899a5ac172ea8
Jostin <figueroajostin4@gmail.com>
2025-05-30 07:56:56 -0500

    actualizacion

--- Talleres/Ejercicio7.py
+++
@@ -1,6 +0,0 @@
-##Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual y el número de años, y muestre por pantalla el capital obtenido en la inversión.
-montoInicial=int(input("Ingrese el monto inicial: "))
-interesAnual=float(input("Ingrese el interes anual: "))/100
-años=int(input("Ingrese el número de años: "))
-capital=montoInicial*(1+interesAnual)**años
-print("El capital obtenido es de",capital)
--- Talleres/ejercicio_5.py
+++ Talleres/ejercicio_5.py
@@ -1,4 +1,5 @@
-##Escribir un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora. Después debe mostrar por pantalla la paga que le corresponde.
+##Escribir un programa que pregunte al usuario por el número de horas trabajadas
+# y el coste por hora. Después debe mostrar por pantalla la paga que le corresponde.
 nombre=input("Ingrese su nombre: ")
 
 horasTrabajadas=float(input("Ingrese el número de horas que trabajó: "))
